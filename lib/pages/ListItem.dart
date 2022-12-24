08d6a0d4766f88f09db46155a161c4dd5239134f
Meilina Anggraeni Putri <meilinaanggraeniputri@gmail.com>
2022-12-24 23:25:15 +0700

    update list

--- lib/pages/ListItem.dart
+++ lib/pages/ListItem.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../DbHelper.dart';
+import '../models/item.dart';
 
 class ListItem extends StatefulWidget {
   const ListItem({super.key});
@@ -10,8 +14,101 @@ class ListItem extends StatefulWidget {
 }
 
 class _CustomListItem extends State<ListItem> {
+  get item => null;
+
+  get itemList => null;
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    // ignore: prefer_conditional_assignment
+    if (itemList == null) {
+      // ignore: deprecated_member_use, prefer_collection_literals
+      itemList = <Item>[];
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daftar Item'),
+      ),
+      body: Column(children: [
+        Expanded(
+          child: createListView(),
+        ),
+        Container(
+          alignment: Alignment.bottomCenter,
+          child: SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              child: Text("Tambah Item"),
+              onPressed: () async {
+                var item = await navigateToEntryForm(context, null);
+                if (item != null) {
+//TODO 2 Panggil Fungsi untuk Insert ke DB
+                  int result = await DBHelper.insert(item);
+                  if (result > 0) {
+                    updateListView();
+                  }
+                }
+              },
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Future<Item> navigateToEntryForm(BuildContext context, Item item) async {
+    var result = await Navigator.push(context,
+        MaterialPageRoute(builder: (BuildContext context) {
+      return EntryForm(item);
+    }));
+    return result;
+  }
+
+  ListView createListView() {
+    TextStyle textStyle = Theme.of(context).textTheme.headline5;
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.red,
+              child: Icon(Icons.ad_units),
+            ),
+            title: Text(
+              this.item[index].name,
+              style: textStyle,
+            ),
+            subtitle: Text(this.item[index].price.toString()),
+            trailing: GestureDetector(
+              child: Icon(Icons.delete),
+              onTap: () async {
+//TODO 3 Panggil Fungsi untuk Delete dari DB berdasarkan Item
+              },
+            ),
+            onTap: () async {
+              var item = await navigateToEntryForm(context, this.item[index]);
+//TODO 4 Panggil Fungsi untuk Edit data
+            },
+          ),
+        );
+      },
+    );
+  }
+
+//update List item
+  void updateListView() {
+    final Future<Database> dbFuture = dbHelper.initDb();
+    dbFuture.then((database) {
+//TODO 1 Select data dari DBFuture<List<Item>> itemListFuture = dbHelper.getItemList();
+      itemListFuture.then((itemList) {
+        setState(() {
+          this.itemList = itemList;
+          this.count = itemList.length;
+        });
+      });
+    });
   }
 }
